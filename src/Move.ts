205ce79292596cfcdc9d532f4d3ed13996aa3800
Amelie Certin <amelie.certin@gmail.com>
2020-03-30 15:50:13 +0200

    Add moves

---
+++ src/Move.ts
@@ -0,0 +1,8 @@
+class Move {
+    constructor(private name: string, private power: number) {
+        this.name = name;
+        this.power = power;
+    }
+}
+
+export { Move };
--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -1,9 +1,20 @@
 import { Stats } from "./Stats"
+import { Move } from "./Move";
 
 class Pokemon {
-    constructor(private name: string, private stats: Stats) {
+    constructor(private name: string, private stats: Stats, private moves?: Move[]) {
         this.name = name;
         this.stats = stats;
+        this.moves = moves ? moves : this.initDefaultMoves();
+    }
+
+    initDefaultMoves() {
+        return [
+            new Move("Bite", 60),
+            new Move("Egg Bomb", 100),
+            new Move("Feint", 30),
+            new Move("Fire Pubch", 100)
+        ];
     }
 
     speed() : number {
